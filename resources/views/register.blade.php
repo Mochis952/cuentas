384c291ec5eea465ead8a2706ecdd387d7dd245f
Gregorio Antonio German Balcazar <gregorioantonio201100@gmail.com>
2024-09-24 23:28:55 -0600

    Agrega area de datos de la cuenta

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.master')
 
 @section('title', 'Registar usuarios')
+<!-- Toastr CSS -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
 @push('styles')
 @endpush
@@ -46,7 +49,7 @@
                 </div>
             </div>
         </div>
-        <div class="row justify-content-center">
+        <div class="row justify-content-center d-none">
             <div class="col-12 col-md-6 col-lg-6">
                 <div class="groupMainTextInput">
                     <label>Perfil</label>
@@ -55,14 +58,14 @@
             </div>
         </div>
         <div class="row justify-content-center">
-            <div class="col-12 col-md-6 col-lg-6">
+            <div class="col-12 col-md-6 col-lg-6 d-none">
                 <div class="groupMainTextInput">
                     <label>Pin</label>
                     <input type="text" id="pin_account" name="pin_account">
                 </div>
             </div>
         </div>
-        <div class="row justify-content-center">
+        <div class="row justify-content-center d-none">
             <div class="col-12 col-md-6 col-lg-6">
                 <div class="groupMainTextInput">
                     <label>Fecha de pago</label>
@@ -73,7 +76,7 @@
         <div class="row justify-content-center">
             <div class="col-12 col-md-6 col-lg-6">
                 <div class="groupMainTextInput">
-                    <button type="submit" id="save_client">Guardar cliente</button>
+                    <button class="success"type="submit" id="save_client" >Guardar cliente</button>
                 </div>
             </div>
         </div>
@@ -83,6 +86,7 @@
 
 @push('scripts')
 <script src="{{ asset('js/jquery.validate.min.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 <script>
     $(document).ready(function() {
     // Configurar la validación del formulario
@@ -101,20 +105,20 @@
             account_streaming: {
                 required: true
             },
-            profile_account: {
-                required: true,
-                minlength: 3
-            },
-            pin_account: {
-                required: true,
-                digits: true,
-                minlength: 4,
-                maxlength: 4
-            },
-            date_payment: {
-                required: true,
-                date: true
-            }
+            // profile_account: {
+            //     required: true,
+            //     minlength: 3
+            // },
+            // pin_account: {
+            //     required: true,
+            //     digits: true,
+            //     minlength: 4,
+            //     maxlength: 4
+            // },
+            // date_payment: {
+            //     required: true,
+            //     date: true
+            // }
         },
         messages: {
             customer_name: {
@@ -130,20 +134,20 @@
             account_streaming: {
                 required: "Por favor, selecciona una cuenta de streaming."
             },
-            profile_account: {
-                required: "Por favor, ingresa el perfil de la cuenta.",
-                minlength: "El perfil debe tener al menos 3 caracteres."
-            },
-            pin_account: {
-                required: "Por favor, ingresa el PIN de la cuenta.",
-                digits: "Solo se permiten números.",
-                minlength: "El PIN debe tener 4 dígitos.",
-                maxlength: "El PIN debe tener 4 dígitos."
-            },
-            date_payment: {
-                required: "Por favor, ingresa la fecha de pago.",
-                date: "Por favor, ingresa una fecha válida."
-            }
+            // profile_account: {
+            //     required: "Por favor, ingresa el perfil de la cuenta.",
+            //     minlength: "El perfil debe tener al menos 3 caracteres."
+            // },
+            // pin_account: {
+            //     required: "Por favor, ingresa el PIN de la cuenta.",
+            //     digits: "Solo se permiten números.",
+            //     minlength: "El PIN debe tener 4 dígitos.",
+            //     maxlength: "El PIN debe tener 4 dígitos."
+            // },
+            // date_payment: {
+            //     required: "Por favor, ingresa la fecha de pago.",
+            //     date: "Por favor, ingresa una fecha válida."
+            // }
         },
         errorElement: 'div',  // El elemento HTML donde se mostrará el error
         errorPlacement: function (error, element) {
@@ -157,6 +161,7 @@
             $(element).removeClass('is-invalid'); // Remover clase de error si está bien
         },
         submitHandler: function(form) {
+            $("#save_client").attr("disabled", true);
             var formData = {
                 customer_name: $("#customer_name").val(),
                 customer_phone_number: $("#customer_phone_number").val(),
@@ -176,14 +181,79 @@
                 data: formData,
                 success: function(response) {
                     console.log("Respuesta del servidor:", response);
+                    setTimeout(() => {
+                        $("#save_client").attr("disabled", false);
+                    }, 5000);
+                    toastr.success('Cliente registrado!', 'Guardado', {
+                        timeOut: 5000, 
+                        positionClass: 'toast-top-right', 
+                        closeButton: true, 
+                        progressBar: true,
+                        "toastClass": "custom-toast-success", 
+                    });
+                    let email="";
+                    let password="";
+                    let perfil="";
+                    let pin ="";
+                    let container_text =    `<div class="row justify-content-center mt-1">
+                                                <div class="col-11 col-md-5 col-lg-5 container_text" >
+                                                    <div id="message_client" class="box-header row">
+                                                        <div class="col-10">
+                                                            <span>Datos de la cuenta del cliente</span>
+                                                        </div>
+                                                        <button class="col-2 bg-transparent border-0" onclick="copy_text(event)">
+                                                        <svg id="copy-button" class="copy-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                                            <path d="M19 21H9c-1.1 0-2-.9-2-2V7h2v12h10v2zm3-16h-8c-1.1 0-2 .9-2 2v10c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 12h-8V7h8v10zM15 3H5c-1.1 0-2 .9-2 2v12h2V5h10V3z"/>
+                                                        </svg>
+                                                        Copiar
+                                                        </button>
+                                                        <br>
+                                                        <span id="text_client" class="col-10 p-0">
+                                                            Correo: ${email} \n 
+                                                            Contraseña: ${password} \n
+                                                            Perfil: ${perfil} \n
+                                                            Pin: ${pin}
+                                                        </span>
+                                                    </div>
+                                                </div>
+                                            </div>`;
+                $("#registrationForm").append(container_text);
                 },
                 error: function(xhr, status, error) {
                     console.error("Error:", error);
+                    $("#save_client").attr("disabled", false);
+                    toastr.error('Error', error, {
+                        timeOut: 5000, 
+                        positionClass: 'toast-top-right', 
+                        closeButton: true, 
+                        progressBar: true,
+                        "toastClass": "custom-toast-error", 
+                    });
+
                 }
             });
         }
     });
 });
+    function copy_text(event) {
+        event.preventDefault(); // Evita que el botón recargue la página
+
+        // Selecciona el contenido del span
+        var $tempElement = $("<textarea>");
+        $("body").append($tempElement);
+        $tempElement.val($("#text_client").text()).select();
+        document.execCommand("copy");
+        $tempElement.remove(); // Elimina el elemento temporal
+
+        // Mostrar mensaje de éxito (opcional)
+        toastr.success('Texto copiado!', 'Copy', {
+                        timeOut: 1000, 
+                        positionClass: 'toast-top-right', 
+                        closeButton: true, 
+                        progressBar: true,
+                        "toastClass": "custom-toast-success", 
+                    });
+    }
 
 </script>
 @endpush
